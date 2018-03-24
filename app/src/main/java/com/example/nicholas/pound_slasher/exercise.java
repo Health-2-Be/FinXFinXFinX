0e82c023a4094b243e4d51dbc6ed20133738242f
NamelessNL <33840334+NamelessNL@users.noreply.github.com>
2018-03-24 15:56:09 -0400

    Add files via upload

--- app/src/main/java/com/example/nicholas/pound_slasher/exercise.java
+++ app/src/main/java/com/example/nicholas/pound_slasher/exercise.java
@@ -1,13 +1,22 @@
 package com.example.nicholas.pound_slasher;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class exercise extends AppCompatActivity {
+    private Button buttonstretches;
+    private Button buttonspeed;
+    private Button buttonstrength;
+    private Button buttonflexibility;
+    private Button buttonendurance;
+    private Button buttonstamina;
+    private Button buttoncoree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,84 @@ public class exercise extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        buttonstretches = (Button) findViewById(R.id.buttonstretches);
+        buttonstretches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesStretches();
+            }
+        });
+        buttonspeed = (Button) findViewById(R.id.buttonspeed);
+        buttonspeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesSpeed();
+            }
+        });
+        buttonstrength = (Button) findViewById(R.id.buttonstrength);
+        buttonstrength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesStrength();
+            }
+        });
+        buttonflexibility = (Button) findViewById(R.id.buttonmflexibility);
+        buttonflexibility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesFlexibility();
+            }
+        });
+        buttonendurance = (Button) findViewById(R.id.buttonendurance);
+        buttonendurance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesEndurance();
+            }
+        });
+        buttonstamina = (Button) findViewById(R.id.buttonstamina);
+        buttonstamina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesStamina();
+            }
+        });
+        buttoncoree = (Button) findViewById(R.id.buttoncoree);
+        buttoncoree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openExercisesCore();
+            }
+        });
+
+    }
+    public void openExercisesStretches(){
+        Intent intent = new Intent(this,exercisesstretches.class);
+        startActivity(intent);
+    }
+    public void openExercisesSpeed(){
+        Intent intent = new Intent(this,exercisesspeed.class);
+        startActivity(intent);
+    }
+    public void openExercisesStrength(){
+        Intent intent = new Intent(this,exercisesstrength.class);
+        startActivity(intent);
+    }
+    public void openExercisesFlexibility(){
+        Intent intent = new Intent(this,exercisesflexibility.class);
+        startActivity(intent);
+    }
+    public void openExercisesEndurance(){
+        Intent intent = new Intent(this,exercisesendurance.class);
+        startActivity(intent);
+    }
+    public void openExercisesStamina(){
+        Intent intent = new Intent(this,exercisesstamina.class);
+        startActivity(intent);
+    }
+    public void openExercisesCore(){
+        Intent intent = new Intent(this,exercisescore.class);
+        startActivity(intent);
     }
 
 }
