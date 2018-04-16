e4b3d3d72c7a3d95f2a9b802a4624632485f4a89
1114085038a <33840148+1114085038a@users.noreply.github.com>
2018-04-15 20:15:06 -0400

    Add files via upload

--- app/src/main/java/com/example/nicholas/pound_slasher/signup.java
+++ app/src/main/java/com/example/nicholas/pound_slasher/signup.java
@@ -23,23 +23,22 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import java.util.regex.Pattern;
 
-public class signup extends AppCompatActivity implements View.OnClickListener{
+public class signup extends AppCompatActivity {
 
-    EditText editTexte,editTextp;
-    ProgressBar progressBar2;
-    private FirebaseAuth Auth;
+    private EditText editTexte,editTextp;
+    private ProgressBar progressBar2;
+    private FirebaseAuth auth;
     private Button buttonjoin;
-    private TextView tlogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        Auth = FirebaseAuth.getInstance();
+        auth = FirebaseAuth.getInstance();
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
 
 
         editTexte =(EditText)findViewById(R.id.editTexte);
@@ -49,86 +48,72 @@ public class signup extends AppCompatActivity implements View.OnClickListener{
         buttonjoin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                registerUser();
+                String email = editTexte.getText().toString().trim();
+                String Password = editTextp.getText().toString().trim();
+                if (email.isEmpty()) {
+                    editTexte.setError("Email is required");
+                    editTexte.requestFocus();
+                    return;
+                }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    editTexte.setError("Email is not Valid");
+                    editTexte.requestFocus();
+                    return;
+                }
+                if (email.isEmpty()) {
+                    editTextp.setError("Password is required");
+                    editTextp.requestFocus();
+                    return;
+                }
+                if (Password.length() < 6) {
+                    editTextp.setError("Please fill in a vaild password");
+                    editTextp.requestFocus();
+                    return;
+                }
+                progressBar2.setVisibility(View.VISIBLE);
+                auth.createUserWithEmailAndPassword(email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar2.setVisibility(View.INVISIBLE);
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "User Registered", Toast.LENGTH_SHORT);
+                        }
+                        else{
+
+                            if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT);
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT);
+                            }
+
+                        }
+
+                    }
+                });
             }
         });
 
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
     }
     private void registerUser() {
-        String email = editTexte.getText().toString().trim();
-        String Password = editTextp.getText().toString().trim();
-
-        if (email.isEmpty()) {
-            editTexte.setError("Email is required");
-            editTexte.requestFocus();
-            return;
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTexte.setError("Email is not Valid");
-            editTexte.requestFocus();
-            return;
-        }
-        if (email.isEmpty()) {
-            editTextp.setError("Password is required");
-            editTextp.requestFocus();
-            return;
-        }
-        if (Password.length() < 6) {
-            editTextp.setError("Please fill in a vaild password");
-            editTextp.requestFocus();
-            return;
-        }
-
-        progressBar2.setVisibility(View.VISIBLE);
 
 
-        Auth.createUserWithEmailAndPassword(email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                progressBar2.setVisibility(View.INVISIBLE);
-                if(task.isSuccessful()){
-                    Toast.makeText(getApplicationContext(), "User Registered", Toast.LENGTH_SHORT);
-                }
-                else{
 
-                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
-                        Toast.makeText(getApplicationContext(), "Registered", Toast.LENGTH_SHORT);
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT);
-                    }
 
-                }
 
-            }
-        });
+
+
+
 
 
 
     }
 
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
 
-            case R.id.buttonjoin:
-                registerUser();
-                break;
-
-
-
-        }
-    }
 }
 
